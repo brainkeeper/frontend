ac381e27257720b08700553fc8314ecbf951ba6f
walli545 <walli5446@googlemail.com>
2019-12-05 21:57:00 +0100

    Add constructor to StorablePerson

--- brainkeeper/src/app/classes/storable-person.ts
+++ brainkeeper/src/app/classes/storable-person.ts
@@ -8,6 +8,12 @@ export class StorablePerson {
     name: string;
     picture: string;
 
+    constructor(id?: number, name?: string, picture?: string) {
+        this.id = id;
+        this.name = name;
+        this.picture = picture;
+    }
+
     /**
      * Converts a Person object to a StorablePerson.
      */
