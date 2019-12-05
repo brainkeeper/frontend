05050bcf8e4bbb822eb9924150e966418ea85005
AndHager <ahager@hm.edu>
2019-12-05 19:40:22 +0100

    Implementation of the session-screen

--- brainkeeper/src/app/app.module.ts
+++ brainkeeper/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridComponent } from './components/session_screen/grid/grid.component';
+import { PersonNameComponent } from './components/session_screen/person-name/person-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    PersonNameComponent
   ],
   imports: [
     BrowserModule,
