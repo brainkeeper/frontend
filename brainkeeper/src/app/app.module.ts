ebafce11d85a8037249106191e00ad12912b000b
walli545 <walli5446@googlemail.com>
2019-12-05 22:01:42 +0100

    Implement database service

--- brainkeeper/src/app/app.module.ts
+++ brainkeeper/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DatabaseService } from './services/database.service';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,9 @@ import {MatIconModule} from '@angular/material/icon';
     MatButtonModule,
     MatIconModule,
   ],
-  providers: [],
+  providers: [
+    DatabaseService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
