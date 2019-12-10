1cc726df35a61733bbc19e23a43a8fd7c67d11b1
AndHager <ahager@hm.edu>
2019-12-10 15:09:49 +0100

    Implementation of the requested changes.

--- brainkeeper/src/app/components/session_screen/grid/grid.component.spec.ts
+++ brainkeeper/src/app/components/session_screen/grid/grid.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GridComponent } from './grid.component';
+import { PersonNameComponent } from './../person-name/person-name.component';
 
 describe('GridComponent', () => {
   let component: GridComponent;
@@ -8,7 +9,7 @@ describe('GridComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GridComponent ]
+      declarations: [ GridComponent, PersonNameComponent ]
     })
     .compileComponents();
   }));
