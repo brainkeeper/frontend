bbb9e7aefd551f797c8a5a94fc18d5ba07eb4356
walli545 <walli5446@googlemail.com>
2019-12-13 08:40:13 +0100

    Add statistics component

--- brainkeeper/src/app/app-routing.module.ts
+++ brainkeeper/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { GridComponent } from './components/session_screen/grid/grid.component';
 import { PersonsComponent } from './components/persons/persons.component';
 import { StartComponent } from './components/start/start.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 
 const routes: Routes = [
   { path: '', component: StartComponent },
   { path: 'persons', component: PersonsComponent },
+  { path: 'statistics', component: StatisticsComponent },
 ];
 
 @NgModule({
--- brainkeeper/src/app/app.module.ts
+++ brainkeeper/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { PersistentPersonService } from './services/persistent-person.service';
 import { PersonService } from './services/person-service';
 import { DatabaseService } from './services/database.service';
+import { StatisticsComponent } from './components/statistics/statistics.component';
 import { PersonsComponent } from './components/persons/persons.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { StartComponent } from './components/start/start.component';
@@ -23,6 +24,7 @@ import { StartComponent } from './components/start/start.component';
     AppComponent,
     GridComponent,
     PersonNameComponent,
+    StatisticsComponent,
     PersonsComponent,
     NavigationBarComponent,
     StartComponent,
