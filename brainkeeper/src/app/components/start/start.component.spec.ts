1411f871daf128d5a7b666f22cf9c5bdb0418309
walli545 <walli5446@googlemail.com>
2019-12-14 10:51:37 +0100

    Create start screen

--- brainkeeper/src/app/components/start/start.component.spec.ts
+++ brainkeeper/src/app/components/start/start.component.spec.ts
@@ -5,17 +5,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
 import { StartComponent } from './start.component';
+import { Router } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 
 describe('StartComponent', () => {
   let component: StartComponent;
   let fixture: ComponentFixture<StartComponent>;
+  let debugElement: DebugElement;
 
   const locationStub = {
     back: jasmine.createSpy('back')
   };
 
+  let routerStub;
+
   beforeEach(async(() => {
+    routerStub = jasmine.createSpyObj('Router', ['navigate']);
     TestBed.configureTestingModule({
       declarations: [
         StartComponent,
@@ -28,6 +35,7 @@ describe('StartComponent', () => {
       ],
       providers: [
         { provide: Location, useValue: locationStub },
+        { provide: Router, useValue: routerStub },
       ],
     })
       .compileComponents();
@@ -36,10 +44,31 @@ describe('StartComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StartComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('click on game button navigates to /game', () => {
+    debugElement.query(By.css('#btn-play')).nativeElement.click();
+    const have = (routerStub.navigate as jasmine.Spy).calls.first().args[0];
+    expect(have).toEqual(['game']);
+  });
+
+  it('click on statistics button navigates to /game', () => {
+    debugElement.query(By.css('#btn-statistics')).nativeElement.click();
+    const have = (routerStub.navigate as jasmine.Spy).calls.first().args[0];
+    expect(have).toEqual(['statistics']);
+  });
+
+  it('click on persons button navigates to /persons', () => {
+    debugElement.query(By.css('#btn-persons')).nativeElement.click();
+    const have = (routerStub.navigate as jasmine.Spy).calls.first().args[0];
+    expect(have).toEqual(['persons']);
+  });
+
+  it('click on settings button navigates to /settings', () => {
+    debugElement.query(By.css('#btn-settings')).nativeElement.click();
+    const have = (routerStub.navigate as jasmine.Spy).calls.first().args[0];
+    expect(have).toEqual(['settings']);
   });
 });
--- brainkeeper/src/app/components/start/start.component.ts
+++ brainkeeper/src/app/components/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  navigateToGame() {
+    this.router.navigate(['game']);
+  }
+
+  navigateToStatistics() {
+    this.router.navigate(['statistics']);
+  }
+
+  navigateToPersons() {
+    this.router.navigate(['persons']);
+  }
+
+  navigateToSettings() {
+    this.router.navigate(['settings']);
+  }
 }
