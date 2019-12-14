f93604dbb819e12687864c4bb3681b223d4a8135
walli545 <walli5446@googlemail.com>
2019-12-14 10:51:03 +0100

    Insert content into navigation bar
    It is now possible to insert e.g. a button into the navigation bar via ng-content.

--- brainkeeper/src/app/components/navigation-bar/navigation-bar.component.spec.ts
+++ brainkeeper/src/app/components/navigation-bar/navigation-bar.component.spec.ts
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { Location } from '@angular/common';
 import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 describe('NavigationBarComponent', () => {
   let component: NavigationBarComponent;
@@ -67,5 +68,39 @@ describe('NavigationBarComponent', () => {
     fixture.detectChanges();
     const title = fixture.debugElement.query(By.css('span')).nativeElement.textContent;
     expect(title).toEqual('Custom Title');
+  });
+
+  it('renders content inside navigation bar', () => {
+    TestBed.resetTestingModule();
+    @Component({
+      selector: 'app-host',
+      template: `
+        <app-navigation-bar>
+          <button id="btn-test" (click)="onClick()"></button>
+        </app-navigation-bar>
+      `,
+    })
+    class TestHostComponent {
+      onClick() {}
+    }
+
+    TestBed.configureTestingModule({
+      declarations: [
+        TestHostComponent,
+        NavigationBarComponent,
+      ],
+      imports: [
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+      ],
+      providers: [
+        { provide: Location, useValue: locationStub },
+      ],
+    }).compileComponents();
+    const host = TestBed.createComponent(TestHostComponent);
+    const clickSpy = spyOn(host.componentInstance, 'onClick');
+    host.debugElement.query(By.css('#btn-test')).nativeElement.click();
+    expect(clickSpy).toHaveBeenCalledTimes(1);
   });
 });
