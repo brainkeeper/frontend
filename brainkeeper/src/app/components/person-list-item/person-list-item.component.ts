f406ce444efb093ea57ca29991b40a81a9cf1e13
Simon Schroeppel <simon.schroeppel@gmail.com>
2019-12-27 22:15:36 +0100

    Create Person-List-Item component
    Each Item shows the picture of the person and his name and score.

---
+++ brainkeeper/src/app/components/person-list-item/person-list-item.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { Person } from 'src/app/classes/person';
+
+@Component({
+  selector: 'app-person-list-item',
+  templateUrl: './person-list-item.component.html',
+  styleUrls: ['./person-list-item.component.scss']
+})
+export class PersonListItemComponent implements OnInit {
+
+  @Input()
+  public person: Person;
+
+  constructor() {
+    this.person = new Person('Omaaaaa', 'assets/testpic.png', 1);
+  }
+
+  ngOnInit() {
+  }
+
+}
