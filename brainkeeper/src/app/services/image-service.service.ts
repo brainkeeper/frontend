a97866b32be6fc8b916e92447f4cc9eda12da632
AndHager <ahager@hm.edu>
2019-12-31 01:57:07 +0100

    Implementation of the resizing and the compression of the image :)

--- brainkeeper/src/app/services/image-service.service.ts
+++ brainkeeper/src/app/services/image-service.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 
-// npm install ng2-img-max blueimp-canvas-to-blob --save
-import { Ng2ImgMaxService } from 'ng2-img-max';
-
 
 @Injectable({
   providedIn: 'root'
@@ -10,87 +7,45 @@ import { Ng2ImgMaxService } from 'ng2-img-max';
 
 export class ImageServiceService {
 
-  constructor(private ng2ImgMax: Ng2ImgMaxService) {  }
+  constructor() {  }
 
-  getBase64(file: File, setter: (base64String: string) => void): void {
+  async getBase64(file: File, modify = true): Promise<string> {
     if (file == null) {
       throw new Error('The input file should not be null.');
     }
-    if (file.type === 'img/png' || file.type === 'img/jpg') {
-      // let resizedImage: File;
-      console.log(' IN SERVICE BEFORE REZISE, ' + file.size);
-      this.ResizeImage(file/*, ( rI ) => { resizedImage = rI; }*/);
-      console.log(' IN SERVICE AFTER REZISE, ' + file.size);
-      this.compress(file);
-    }
+    return new Promise<string>( async (resolve, reject) => {
 
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
 
-    reader.onload = () => {
-      const result = reader.result.toString();
-      setter(result);
-    };
+      reader.onload = () => {
+        let result = reader.result.toString();
+        if ((file.type === 'png' || file.type === 'jpg') && modify) {
+          const img = new Image();
+          img.src = result;
+          result = this.changeImage(img);
+          // console.log(' ### RES2 ### , ' + result + ', ' + result.length + ', old file: ' + file.size);
+        }
+        resolve(result);
+      };
 
-    reader.onerror = () => {
-      throw new Error('In the image conversion went something wrong.');
-    };
+      reader.onerror = () => {
+        throw new Error('In the image conversion went something wrong.');
+      };
+    });
   }
 
-  ResizeImage(file: File/*, setter: (resizedImage: File) => void*/): void {
-    /*
-    const img = document.createElement('img');
-    img.src = URL.createObjectURL(file);
-
+  changeImage(img: any): string {
     const canvas = document.createElement('canvas');
+
+    const width = 400;
+    const height = 400;
+
+    canvas.width = width;
+    canvas.height = height;
     const ctx = canvas.getContext('2d');
-
-    const MAX_WIDTH = 400;
-    const MAX_HEIGHT = 400;
-    const width = img.width;
-    const height = img.height;
-
-    canvas.width = width > MAX_WIDTH ? MAX_WIDTH : width;
-    canvas.height = height > MAX_HEIGHT ? MAX_HEIGHT : height;
-
     ctx.drawImage(img, 0, 0, width, height);
 
-    console.log('**CANVAS**: ' + canvas.toDataURL());
-
-    return canvas.toDataURL(file.type);
-    */
-    console.log('**IMAGE RESIZER**: was called');
-    const WIDTH = 400;
-    const HEIGHT = 400;
-    this.ng2ImgMax.resizeImage(file, WIDTH, HEIGHT).subscribe((result) => {
-        console.log(result);
-      }, error => {
-        console.error(error);
-      }
-    );
-    /*
-    .subscribe(
-      result => {
-        console.log('**IMAGE RESIZER**: ' + result);
-        setter(result);
-      },
-      () => {
-        throw new Error('In the image rsize progress went something wrong.');
-      }
-    );*/
+    return canvas.toDataURL('image/jpeg', 0.7); // get the data from canvas as 70% JPG (can be also PNG, etc.)
   }
-
-  compress(file: File/*, setter: (resizedImage: File) => void*/): void {
-
-    console.log('**IMAGE RESIZER**: was called');
-    const WIDTH = 400;
-    const HEIGHT = 400;
-    this.ng2ImgMax.compress([file], (file.size * 100) / 75).subscribe((result) => {
-        console.log(result);
-      }, error => {
-        console.error(error);
-      }
-    );
-  }
-
 }
