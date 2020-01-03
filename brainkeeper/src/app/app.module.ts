6c24894c7daf02c5a46e6322696e29d4d05dc668
AndHager <ahager@hm.edu>
2020-01-03 20:21:47 +0100

    Inproved and tested the service

--- brainkeeper/src/app/app.module.ts
+++ brainkeeper/src/app/app.module.ts
@@ -14,12 +14,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { PersistentPersonService } from './services/persistent-person.service';
 import { PersonService } from './services/person-service';
 import { DatabaseService } from './services/database.service';
-// npm install ng2-img-max blueimp-canvas-to-blob --save
-import { Ng2ImgMaxModule } from 'ng2-img-max';
-// npm install ng2-pica --save
-import { Ng2PicaModule } from 'ng2-pica';
-// npm install --save-dev @types/node
-import * as FS from 'fs';
 
 @NgModule({
   declarations: [
@@ -34,8 +28,6 @@ import * as FS from 'fs';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    Ng2ImgMaxModule,
-    Ng2PicaModule,
   ],
   providers: [
     { provide: PersonService, useClass: PersistentPersonService, },
--- brainkeeper/src/app/services/image-service.service.ts
+++ brainkeeper/src/app/services/image-service.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { ImgExifService } from 'ng2-img-max';
 
 
 @Injectable({
@@ -10,24 +9,33 @@ export class ImageServiceService {
 
   constructor() {  }
 
-  async getBase64(file: File, modify = true): Promise<string> {
+  async getBase64(
+    file: File, width: number = 0, height: number = 0, quality: number = 1.0
+    ): Promise<string> {
+
     if (file == null) {
       throw new Error('The input file should not be null.');
     }
+    if (quality < 0 || quality > 1){
+      throw new Error('The quality sould be between 0 and 1, but it was: ' + quality);
+    }
+    if (width < 0) {
+      throw new Error('The width should be positive, but it was: ' + width);
+    }
+    if (height < 0) {
+      throw new Error('The height should be positive, but it was: ' + height);
+    }
+
     return new Promise<string>( async (resolve, reject) => {
 
       const reader = new FileReader();
       reader.readAsDataURL(file);
 
       reader.onload = async () => {
-        let result = reader.result.toString();
-        if ((file.type === 'png' || file.type === 'jpg') && modify) {
-          const img = new Image();
-          img.src = result;
-          result = await this.changeImage(img);
-          console.log(' ### RES2 ###' + result);
-        }
-        resolve(result);
+        const img = new Image();
+        img.src = reader.result.toString();
+
+        resolve(await this.changeImage(img, file.type, width, height, quality));
       };
 
       reader.onerror = () => {
@@ -36,68 +44,50 @@ export class ImageServiceService {
     });
   }
 
-  async changeImage(img: HTMLImageElement): Promise<string> {
-    /*
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
+  async changeImage(
+    img: HTMLImageElement, type: string, width: number, height: number, quality: number
+    ): Promise<string> {
+    // console.log('type1: ' + type + ',  width: ' + width + ', height: ' + height + ', quality: ' + quality);
 
-    const oc = document.createElement('canvas');
-    const octx = oc.getContext('2d');
+    if (width === 0) {
+      width = img.width;
+    }
+    if (height === 0) {
+      height = img.height;
+    }
 
-    
+    // console.log('type2: ' + type + ',  width: ' + width + ', height: ' + height + ', quality: ' + quality);
 
-    img.width = width;
-    img.height = height;
-    console.log('*** IMG SRC ***' + img.src.length);
-
-    canvas.width = width;
-    canvas.height = height;
-
-    oc.width = width;
-    oc.height = height;
-    octx.drawImage(oc, 0, 0, width, height);
-
-    ctx.drawImage(img, 0, 0, width, height);
-    console.log(oc.toDataURL());
-    return canvas.toDataURL('image/png', 0.5).toString(); // get the data from canvas as 50% PNG (can be also jpg, etc.)
-    */
-    const width = 400;
-    const height = 400;
     return new Promise<string>((resolve, reject) => {
-    img.onload = () => {
-      if ( img.height <= height && img.width <= width) {
-        return img.src;
+      img.onload = () => {
+        const resizingCanvas: HTMLCanvasElement = document.createElement('canvas');
+        const resizingCanvasContext = resizingCanvas.getContext('2d');
 
-        // TODO: Call method to do something with the resize image
-      } else {
-
-          const resizingCanvas: HTMLCanvasElement = document.createElement('canvas');
-          const resizingCanvasContext = resizingCanvas.getContext('2d');
-
-          // Start with original image size
-          resizingCanvas.width = img.width;
-          resizingCanvas.height = img.height;
+        // Start with original image size
+        resizingCanvas.width = img.width;
+        resizingCanvas.height = img.height;
 
 
-          // Draw the original image on the (temp) resizing canvas
-          resizingCanvasContext.drawImage(img, 0, 0, resizingCanvas.width, resizingCanvas.height);
+        // Draw the original image on the (temp) resizing canvas
+        resizingCanvasContext.drawImage(img, 0, 0,
+          resizingCanvas.width, resizingCanvas.height);
 
-          const curImageDimensions = {
-              width: Math.floor(img.width),
-              height: Math.floor(img.height)
-          };
+        const curImageDimensions = {
+            width: Math.floor(img.width),
+            height: Math.floor(img.height)
+        };
 
-          const outputCanvas: HTMLCanvasElement = document.createElement('canvas');
-          const outputCanvasContext = outputCanvas.getContext('2d');
+        const outputCanvas: HTMLCanvasElement = document.createElement('canvas');
+        const outputCanvasContext = outputCanvas.getContext('2d');
 
-          outputCanvas.width = width;
-          outputCanvas.height = height;
+        outputCanvas.width = width;
+        outputCanvas.height = height;
 
-          outputCanvasContext.drawImage(resizingCanvas, 0, 0, curImageDimensions.width, curImageDimensions.height,
-              0, 0, width, height);
+        outputCanvasContext.drawImage(resizingCanvas, 0, 0,
+           curImageDimensions.width, curImageDimensions.height,
+            0, 0, width, height);
 
-          resolve(outputCanvas.toDataURL('image/jpeg', 0.75));
-      }
+        resolve(outputCanvas.toDataURL('image/jpeg', quality));
     };
   });
   }
