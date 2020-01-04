d0590a25962f198dfa90e9c3339e3d0c192a3666
AndHager <ahager@hm.edu>
2020-01-04 15:16:35 +0100

    Finaly found and fixed the bug

--- brainkeeper/src/app/services/image-service.service.ts
+++ brainkeeper/src/app/services/image-service.service.ts
@@ -10,13 +10,13 @@ export class ImageServiceService {
   constructor() {  }
 
   async getBase64(
-    file: File, width: number = 0, height: number = 0, quality: number = 1.0
+    file: File, width: number = 0, height: number = 0, quality: number = 1
     ): Promise<string> {
 
     if (file == null) {
       throw new Error('The input file should not be null.');
     }
-    if (quality < 0 || quality > 1){
+    if (quality < 0 || quality > 1) {
       throw new Error('The quality sould be between 0 and 1, but it was: ' + quality);
     }
     if (width < 0) {
@@ -26,73 +26,45 @@ export class ImageServiceService {
       throw new Error('The height should be positive, but it was: ' + height);
     }
 
-    return new Promise<string>( (resolve, reject) => {
-
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-
+    const img = new Image();
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    return await new Promise<string>( (resolve, reject) => {
       reader.onload = () => {
-        const img = new Image();
-        img.src = reader.result.toString();
+        const dataURL = reader.result.toString();
+        img.src = dataURL;
 
-        const result = this.changeImage(img, file.type, width, height, quality);
-
-        // console.log(result);
-
-        resolve(result);
+        if (width === 0) {
+          width = img.width;
+        }
+        if (height === 0) {
+          height = img.height;
+        }
+        resolve(this.changeImage(img, file.type === 'jpg' ? 'jpeg' : 'png', width, height, quality));
       };
-
       reader.onerror = () => {
         throw new Error('In the image conversion went something wrong.');
       };
     });
   }
 
-  async changeImage(
+  changeImage(
     img: HTMLImageElement, type: string, width: number, height: number, quality: number
     ): Promise<string> {
-    // console.log('type1: ' + type + ',  width: ' + width + ', height: ' + height + ', quality: ' + quality);
-
-    if (width === 0) {
-      width = img.width;
-    }
-    if (height === 0) {
-      height = img.height;
-    }
-
-    // console.log('type2: ' + type + ',  width: ' + width + ', height: ' + height + ', quality: ' + quality);
-
-    return await new Promise<string>((resolve, reject) => {
+    return new Promise<string>((resolve, reject) => {
       img.onload = () => {
         const resizingCanvas: HTMLCanvasElement = document.createElement('canvas');
         const resizingCanvasContext = resizingCanvas.getContext('2d');
 
-        // Start with original image size
         resizingCanvas.width = img.width;
         resizingCanvas.height = img.height;
 
-
-        // Draw the original image on the (temp) resizing canvas
         resizingCanvasContext.drawImage(img, 0, 0,
           resizingCanvas.width, resizingCanvas.height);
 
-        const curImageDimensions = {
-            width: Math.floor(img.width),
-            height: Math.floor(img.height)
-        };
-
-        const outputCanvas: HTMLCanvasElement = document.createElement('canvas');
-        const outputCanvasContext = outputCanvas.getContext('2d');
-
-        outputCanvas.width = width;
-        outputCanvas.height = height;
-
-        outputCanvasContext.drawImage(resizingCanvas, 0, 0,
-           curImageDimensions.width, curImageDimensions.height,
-            0, 0, width, height);
-
-        resolve(outputCanvas.toDataURL('image/jpeg', quality));
-    };
-  });
+        const response = resizingCanvas.toDataURL('image/' + type, quality);
+        resolve(response);
+      };
+    });
   }
 }
