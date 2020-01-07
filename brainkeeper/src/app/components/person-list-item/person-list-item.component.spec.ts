cc27f05680e285c26b3802b369c9027c5443235c
Simon Schroeppel <simon.schroeppel@gmail.com>
2020-01-07 21:02:13 +0100

    Add Tests to Person list item
    Can't run tests locally as karma doesn't like my angular environment

--- brainkeeper/src/app/components/person-list-item/person-list-item.component.spec.ts
+++ brainkeeper/src/app/components/person-list-item/person-list-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonListItemComponent } from './person-list-item.component';
+import {Person} from '../../classes/person';
 
 describe('PersonListItemComponent', () => {
   let component: PersonListItemComponent;
@@ -21,5 +22,14 @@ describe('PersonListItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display correct data about a person', () => {
+    fixture.whenStable();
+    component.person = new Person('Oma', 'src/assets/rentner_test.png');
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('div.container>img.picture').src).toContain('src/assets/rentner_test.png');
+    expect(compiled.querySelector('div.container>div.person_name')).toContain('Oma');
   });
 });
